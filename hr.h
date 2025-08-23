af125075ded216ecc3c65c0fefec7c57e56f0a3c
Giorgio Massarini <154694911+GiorgioMassarini@users.noreply.github.com>
2025-08-23 16:58:25 +0200

    Delete hr.h

--- hr.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HR_H
-#define _HR_H
-
-#include "list.h"
-
-list leggi_posizioni(char* fileName);
-Candidato* leggi_candidati(FILE* file, int* dim);
-Candidato* trova_candidati(char* p_id, list p, Candidato* c, int dim_c, int* dim);
-#endif
